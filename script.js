111b384da78ef501ae324f3e545156cb7787fea4
Paula Manese <41752060+PaulaManese@users.noreply.github.com>
2023-05-18 14:42:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "1 Jan 2023";
+const newYears = "1 Jan 2024";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
